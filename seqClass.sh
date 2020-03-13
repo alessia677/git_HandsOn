4477c1082d6772b863e1be3767e79b33b1c47144
Ferdinando Squitieri <ferdinandosquitieri@MacBook-Air-di-Ferdinando-2.local>
2020-03-13 11:02:54 +0100

    changes

--- seqClass.sh
+++ seqClass.sh
@@ -4,6 +4,7 @@ seq=$(echo $seq | tr a-z A-Z)  # Note we just added this line
 if [[ $seq =~ ^[ACGTU]+$ ]]; then
   if [[ $seq =~ T ]]; then
     echo "The sequence is DNA"
+""This function detects if a sequence is DNA or RNA""
   elif [[ $seq =~ U ]]; then
     echo "The sequence is RNA"
   else
@@ -11,4 +12,14 @@ if [[ $seq =~ ^[ACGTU]+$ ]]; then
   fi
 else
   echo "The sequence is not DNA nor RNA"
+fi
+""This implement of the function allow the user to find motifs in the sequence""
+motif=$(echo $2 | tr a-z A-Z)
+if [[ -n $motif ]]; then
+  echo -en "Motif search enabled: looking for motif '$motif' in sequence '$seq'... "
+  if [[ $seq =~ $motif ]]; then
+    echo "FOUND"
+  else
+    echo "NOT FOUND"
+  fi
 fi
